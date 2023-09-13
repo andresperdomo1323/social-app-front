aa9c222e34018832a1effc261a6cec5dbd21606a
LauraOrozco26 <141424974+LauraOrozco26@users.noreply.github.com>
2023-09-12 19:38:42 -0500

    login 1
    Creación de los inputs de contraseña y correo

---
+++ social-app/src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    // You can perform additional initialization here if needed
+  }
+
+  onSubmit() {
+    if (this.loginForm) {
+      const usernameControl = this.loginForm.get('username');
+      const passwordControl = this.loginForm.get('password');
+
+      if (usernameControl && passwordControl) {
+        const username = usernameControl.value;
+        const password = passwordControl.value;
+
+        // Now you can use username and password
+        // ...perform your login logic here
+      }
+    }
+  }
+}
